c819eb9351b553589974fee35289d12bf4b9b814
leolucena22 <leonardolucena44@gmail.com>
2020-07-15 14:43:43 -0300

    Aula 14 quase feita

---
+++ Aula14/src/aula14/Aula14.java
@@ -0,0 +1,9 @@
+package aula14;
+
+
+public class Aula14 {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Aula14/src/aula14/Gafanhoto.java
@@ -0,0 +1,27 @@
+package aula14;
+
+public class Gafanhoto extends Pessoa {
+    private String login;
+    private int totAssistido;
+    
+    public void viuMaisUm() {
+        setTotAssistido(getTotAssistido() + 1); 
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotAssistido() {
+        return totAssistido;
+    }
+
+    public void setTotAssistido(int totAssistido) {
+        this.totAssistido = totAssistido;
+    } 
+    
+}
